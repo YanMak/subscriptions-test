d6085918e68d786e0e409e3dff2dcda0f2e916ca
YanMak <89269167763@yandex.ru>
2025-08-09 21:22:54 +0300

    still fixing double db tags

--- libs/pkg/decoder/decoder.go
+++ libs/pkg/decoder/decoder.go
@@ -20,7 +20,7 @@ type FieldInfo struct {
 }
 
 type StructMeta struct {
-	FieldsByTag map[string]map[string]*[]FieldInfo // tagName -> tagValue -> FieldInfo
+	FieldsByTag map[string]map[string][]*FieldInfo // tagName -> tagValue -> FieldInfo
 	Fields      []*FieldInfo
 }
 
@@ -34,7 +34,7 @@ func (m *DecoderService) GetStructMeta(t reflect.Type) *StructMeta {
 	}
 
 	meta := &StructMeta{
-		FieldsByTag: make(map[string]map[string]*[]FieldInfo),
+		FieldsByTag: make(map[string]map[string][]*FieldInfo),
 		Fields:      make([]*FieldInfo, 0, t.NumField()),
 	}
 
@@ -65,7 +65,7 @@ func (m *DecoderService) GetStructMeta(t reflect.Type) *StructMeta {
 
 		for tagKey, tagVal := range tags {
 			if meta.FieldsByTag[tagKey] == nil {
-				meta.FieldsByTag[tagKey] = make(map[string]*[]FieldInfo)
+				meta.FieldsByTag[tagKey] = make(map[string][]*FieldInfo)
 			}
 			meta.FieldsByTag[tagKey][tagVal] = append(meta.FieldsByTag[tagKey][tagVal], fi)
 		}
@@ -76,7 +76,7 @@ func (m *DecoderService) GetStructMeta(t reflect.Type) *StructMeta {
 
 func (m *DecoderService) MustGetFieldByTag(t reflect.Type, tagKey, tagValue string) (*FieldInfo, error) {
 	meta := m.GetStructMeta(t)
-	if fis, ok := meta.FieldsByTag[tagKey][tagValue]; ok && len(*fis) > 0 {
+	if fis, ok := meta.FieldsByTag[tagKey][tagValue]; ok && len(fis) > 0 {
 		return fis[0], nil
 	}
 	return nil, fmt.Errorf("field with tag %s=\"%s\" not found in type %s", tagKey, tagValue, t.Name())
